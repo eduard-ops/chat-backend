2efe4cd94dcad2dd67b36386eb9d5add3a154477
eduard-ops <epustynnik@gmail.com>
2022-09-22 17:21:34 +0300

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { join } from 'path';
 import { AppGateway } from './app.gateway';
-import { MessagesEntity } from './messages.entity';
+// import { MessagesEntity } from './messages.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'messages',
-      username: 'postgres',
-      password: 'naruto13',
-      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-      migrations: [join(__dirname, '**', '*.migration.{ts,js}')],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([MessagesEntity]),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   database: 'messages',
+    //   username: 'postgres',
+    //   password: 'naruto13',
+    //   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    //   migrations: [join(__dirname, '**', '*.migration.{ts,js}')],
+    //   synchronize: true,
+    // }),
+    // TypeOrmModule.forFeature([MessagesEntity]),
   ],
   controllers: [],
   providers: [AppGateway],
