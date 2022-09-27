26c9f06afca2ee3683cc6e96b4732ec866500a7c
eduard-ops <epustynnik@gmail.com>
2022-09-27 16:25:53 +0300

    git push

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -10,13 +10,28 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
 
   @SubscribeMessage("msgToServer")
   handleMessage(client: Socket, payload: string): void {
+    console.log(payload);
     this.server.emit("msgToClient", payload, client.id);
   }
 
-  // @SubscribeMessage('userToServer')
-  // handleUsers(client: Socket, payload: string): void {
-  //   console.log()
-  // }
+  @SubscribeMessage("addUser")
+  handleUsers(client: Socket, payload: string): void {
+    this.addUsers(payload, client.id);
+    this.server.emit("getUsers", this.users);
+  }
+
+  addUsers(payload: string, client: string): void {
+    const { id, name }: any = payload;
+    !this.users.some(user => user.id === id) && this.users.push({ id, name, soketId: client });
+  }
+
+  removeUser(socketId: string): void {
+    this.users = this.users.filter(user => user.soketId !== socketId);
+  }
+
+  addUser(userId: string) {
+    return this.users.find(user => user.id === userId);
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   afterInit(server: Server) {
@@ -28,6 +43,8 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
   }
 
   handleDisconnect(client: Socket) {
+    this.removeUser(client.id);
+    this.server.emit("getUsers", this.users);
     // this.logger.log(`Client disconnected: ${client.id}`);
   }
 }
